7aaf22842ce334423cdacdfcedc9783b708456e0
SilvaEmerson <emersonjose877@gmail.com>
2019-03-28 22:42:03 -0300

    Replace with Promises

--- src/js/objects.js
+++ src/js/objects.js
@@ -11,16 +11,15 @@ const fbxLoader = new FBXLoader();
 
 mtlLoader.setTexturePath('../assets/models/');
 
-export const loadScenario = scene => (
-    mtlLoader.load('../assets/models/cenario exagono.mtl', (materials) => {
+export const loadScenario = () => new Promise((resolve, reject) => (
+    mtlLoader.load('../assets/models/cenario exagono.mtl', materials => {
       materials.preload()
       objLoader.setMaterials(materials)
-      objLoader.load('../assets/models/cenario exagono.obj', (object) => {
-        console.log(object)
-        scene.add(object)
+      objLoader.load('../assets/models/cenario exagono.obj', object => {
+        resolve(object)
       })
     })
-);
+));
 
 
 export const loadWalking = (scene, objs) => ( 
@@ -36,19 +35,13 @@ export const loadWalking = (scene, objs) => (
 )
 
 
-export const loadSword = scene => (
+export const loadSword = () => new Promise((resolve, reject) => (
     mtlLoader.load('../assets/models/chr_sword.mtl', (materials) => {
       materials.preload()
       objLoader.setMaterials(materials)
       objLoader.load('../assets/models/chr_sword.obj', (object) => {
-        scene.add(object);
-        object.position.x = 0
-        object.position.y = 0
-        object.position.z = 0
-        object.scale.x = 10
-        object.scale.y = 10
-        object.scale.z = 10
+        resolve(object);
       })
     })
-)
+));
 
